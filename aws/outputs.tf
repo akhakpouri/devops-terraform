5e4ca84cfa9210bcd09bb416ac6bea64d12faa17
Ali Khakpouri <ali.khakpouri@gmail.com>
2024-07-16 15:31:19 -0400

    completed the aws resource creation

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -1,9 +1,9 @@
 output "instance_id" {
-  description = "Id of the ec2 instance"
-  value       = aws_instance.app_server.id
+  description = "s3 bucket versioning"
+  value       = aws_s3_bucket_versioning.terraform_bucket_versioning
 }
 
 output "instance_public_ip" {
-  description = "public ip address of the ec2 instance"
-  value       = aws_instance.app_server.public_ip
+  description = "dynamodb's lock"
+  value       = aws_dynamodb_table.terraform_locks
 }
